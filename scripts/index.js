4c1d67520f3eb8a8c4ad08fd3a01a091fe36bfd4
Alina Oblysova <obl-al@yandex.ru>
2024-03-25 01:17:50 +0300

    first attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardsContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function addCard(cardName, cardLink) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardElement.querySelector('.card__image').src = cardLink;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', evt => removeCard(evt.target));
+  cardsContainer.append(cardElement);
+}
 // @todo: Функция удаления карточки
-
+function removeCard(clickedButton) {
+  const cardToBeRemoved = clickedButton.closest('.card');
+  cardToBeRemoved.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => addCard(card.name, card.link));
