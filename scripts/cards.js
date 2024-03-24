08fcf180257d527de0e68e34e0efedc080b5ec65
Alina Oblysova <obl-al@yandex.ru>
2024-03-25 02:47:31 +0300

    second attempt

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Я не понимаю, почему я должна сидеть и выдумывать альтернативные подписи к картинкам, но на этой изображены горы",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Если бы выдумывание подписей к картинкам было зафиксировано в задании, я бы, возможно, не описывала озеро в зимнем лесу таким образом",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Из-за таких неточных и неполных формулировок хочется спрыгнуть с крыши изображенного панельного дома",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "На этом моя фантазия закончилась, здесь вечная мерзлота",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "А вот с рельсами в лесу Холмогорского района можно придумать кое-что интересное. Из-за неточных и неполных формулировок в задании хочется прыгнуть еще и под идущий по упомянутым рельсам поезд",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Здесь все предсказуемо. Задания в Практикуме – спонсор вашего прыжка с заснеженных возвышенностей прямиком в замерзший Байкал (головой вниз)",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,21 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardsContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addCard(cardName, cardLink) {
+function createCard(initialCard, removeCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__title').textContent = cardName;
-  cardElement.querySelector('.card__image').src = cardLink;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', evt => removeCard(evt.target));
-  cardsContainer.append(cardElement);
+  cardElement.querySelector('.card__title').textContent = initialCard.name;
+  cardElement.querySelector('.card__image').src = initialCard.link;
+  cardElement.querySelector('.card__image').alt = initialCard.alt;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+  return cardElement;
 }
 // @todo: Функция удаления карточки
-function removeCard(clickedButton) {
-  const cardToBeRemoved = clickedButton.closest('.card');
+function removeCard(evt) {
+  const cardToBeRemoved = evt.target.closest('.card');
   cardToBeRemoved.remove();
 }
 // @todo: Вывести карточки на страницу
-initialCards.forEach(card => addCard(card.name, card.link));
+initialCards.forEach(initialCard => {
+  const card = createCard(initialCard, removeCard);
+  cardsContainer.append(card);
+});5
